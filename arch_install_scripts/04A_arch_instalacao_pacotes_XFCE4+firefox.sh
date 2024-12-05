2da204aded8dd5c2563eefc9fdd934e64a4dafa7
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 10:05:52 -0300

    incluindo tempos de espera

--- arch_install_scripts/04A_arch_instalacao_pacotes_XFCE4+firefox.sh
+++ arch_install_scripts/04A_arch_instalacao_pacotes_XFCE4+firefox.sh
@@ -2,6 +2,11 @@
 
 #pacotes para usar o xfce já com pacotes do thunar e temas
 echo Instalando pacotes para usar o xfce já com pacotes do thunar e temas --
+
+sleep 5s
+
 pacman -S xfce4 xfce4-goodies xfce4-terminal xfce4-goodies gvfs thunar-volman thunar-archive-plugin xfwm4-themes firefox
 
 echo ----fim instalação pacotes XFCE4 --------------------------------------
+
+sleep 5s
