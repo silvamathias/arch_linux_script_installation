034f2bd6cbb5545f17e5eda10ef348b10c52374a
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 12:31:09 -0300

    incluindo tempos de espera

--- arch_install_scripts/04C_arch_instalacao_pacotes_KDE+firefox.sh
+++ arch_install_scripts/04C_arch_instalacao_pacotes_KDE+firefox.sh
@@ -3,11 +3,19 @@
 #pacotes para usar o KDE Plasma
 echo Instalando pacotes KDE Plasma ----------------------------------------------
 
+sleep 5s
+
 pacman -S plasma firefox
 
 # Descomente a linha de baixo para instalar outros pacotes kde-applications (jogos e outros apps)
 #pacman -S kde-applications
 
-echo ----fim instalação pacotes KDE Plasma --------------------------------------
+
+
+sleep 1s
 
 systemctl enable sddm
+
+echo ----fim instalação pacotes KDE Plasma --------------------------------------
+
+sleep 5s
