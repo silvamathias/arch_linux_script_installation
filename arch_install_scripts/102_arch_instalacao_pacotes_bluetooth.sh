d5f7d3ddbc88e491a1d866f738d7e3d31a08fd7a
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 13:43:30 -0300

    incluindo tempos de espera

--- arch_install_scripts/102_arch_instalacao_pacotes_bluetooth.sh
+++ arch_install_scripts/102_arch_instalacao_pacotes_bluetooth.sh
@@ -2,10 +2,15 @@
 
 #bluetooth
 echo Instalando pacotes de Bluetooth ---------------------------------------
+
+sleep 5s
+
 pacman -S bluez bluez-utils gnome-bluetooth-3.0 blueman 
 
 echo ----fim instalação pacotes de Bluetooth -------------------------------
 
 echo iniciando o serviço ---------------------------------------------------
+
+sleep 5s
 
 systemctl enable bluetooth
