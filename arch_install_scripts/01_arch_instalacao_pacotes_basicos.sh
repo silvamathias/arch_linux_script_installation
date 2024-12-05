d070575a4bc59b5450f4443bf95388dbd1167813
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 09:52:07 -0300

    incluindo tempos de espera

--- arch_install_scripts/01_arch_instalacao_pacotes_basicos.sh
+++ arch_install_scripts/01_arch_instalacao_pacotes_basicos.sh
@@ -2,19 +2,31 @@
 
 #pacotes essenciais
 echo Pacotes essenciais
+
+sleep 5s
+
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano vim
+
+sleep 1s
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
+sleep 1s
+
 arch-chroot /mnt
 
-
+sleep 1s
 #pacotes uteis e de internet
 echo Instalando pacotes uteis e de internet --------------------------------
 
+sleep 4s
+
 pacman -S sudo file-roller dosfstools os-prober mtools network-manager-applet networkmanager wpa_supplicant wireless_tools dialog inxi htop neofetch
+
+sleep 1s
 
 systemctl enable dhcpcd
 
 echo ----fim instalação pacotes uteis e de internet ------------------------
 
+sleep 5s
