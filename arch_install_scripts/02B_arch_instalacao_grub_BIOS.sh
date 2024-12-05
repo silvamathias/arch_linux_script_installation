538330dca83ab6808847af4a2e07ab250024c89c
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 09:54:24 -0300

    incluindo tempos de espera

--- arch_install_scripts/02B_arch_instalacao_grub_BIOS.sh
+++ arch_install_scripts/02B_arch_instalacao_grub_BIOS.sh
@@ -4,6 +4,9 @@
 #instalando o grub bios-legacy
 
 echo instalando o grub BIOS-Legacy---------------------------------------
+
+sleep 5s
+
 pacman -S grub
 
 grub-install --target=i386-pc --recheck /dev/sda
@@ -11,3 +14,5 @@ grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo ----fim instalação do grub -------------------------------------------
+
+sleep 5s
