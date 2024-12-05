2c7cc1f8de605f900837e557fe9168ab6bd935c7
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 13:44:43 -0300

    incluindo tempos de espera

--- arch_install_scripts/103_arch_instalacao_pacotes_audio.sh
+++ arch_install_scripts/103_arch_instalacao_pacotes_audio.sh
@@ -3,9 +3,14 @@
 #pacotes para audio
 echo Instalando pacotes para audio -----------------------------------------
 
+sleep 5s
+
 pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-jack pulseaudio-alsa sof-firmware pavucontrol
 
 echo ----fim instalação pacotes para audio ---------------------------------
 
 echo iniciando serviço -----------------------------------------------------
+
+sleep 5s
+
 sulseaudio –start
