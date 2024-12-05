e67653429e31dc5c66fbcb6e3dd9f64b39db3498
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 12:36:54 -0300

    incluindo tempos de espera

--- arch_install_scripts/05A_arch_instalacao_pacote_gerenciador_login_SDDM.sh
+++ arch_install_scripts/05A_arch_instalacao_pacote_gerenciador_login_SDDM.sh
@@ -2,10 +2,15 @@
 
 #gerenciador de login
 echo Instalando gerenciador de login ---------------------------------------
+
+sleep 5s
+
 pacman -S sddm 
 
 echo ----fim instalação do gerenciador de login ----------------------------
 
 echo iniciando o serviço ---------------------------------------------------
+
+sleep 5s
 
 systemctl enable sddm
