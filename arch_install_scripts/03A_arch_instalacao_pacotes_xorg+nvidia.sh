2bc86d46e2a5bca849f5084a16ef2bf9749dcdac
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 09:57:06 -0300

    incluindo tempos de espera

--- arch_install_scripts/03A_arch_instalacao_pacotes_xorg+nvidia.sh
+++ arch_install_scripts/03A_arch_instalacao_pacotes_xorg+nvidia.sh
@@ -3,8 +3,11 @@
 #instalando interface gráfica (xorg + Nvidia)
 echo instalando interface gráfica (xorg + Nvidia) --------------------------
 
+sleep 5s
+
 pacman -S xorg-server xorg-xinit xorg-apps mesa
 
 pacman -S nvidia nvidia-settings
 
 echo ----fim da instalando interface gráfica (xorg + Nvidia) ---------------
+sleep 5s
