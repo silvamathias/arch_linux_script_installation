6915192f2c83f024dea8e985acf75f8f1f48544a
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 13:41:52 -0300

    incluindo tempos de espera

--- arch_install_scripts/05B_arch_instalacao_pacote_gerenciador_login_ligthDM.sh
+++ arch_install_scripts/05B_arch_instalacao_pacote_gerenciador_login_ligthDM.sh
@@ -2,10 +2,14 @@
 
 #gerenciador de login
 echo Instalando gerenciador de login ---------------------------------------
+
+sleep 5s
+
 pacman -S lightdm 
 
 echo ----fim instalação do gerenciador de login ----------------------------
-
 echo iniciando o serviço ---------------------------------------------------
+
+sleep 5s
 
 systemctl enable lightdm.service
