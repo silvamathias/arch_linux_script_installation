3665ea5c9b4ef00045c8e00923332b6f22399324
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 09:53:25 -0300

    incluindo tempos de espera

--- arch_install_scripts/02A_arch_instalacao_grub_uefi.sh
+++ arch_install_scripts/02A_arch_instalacao_grub_uefi.sh
@@ -4,6 +4,9 @@
 #instalando o grub uefi
 
 echo instalando o grub UEFI--------------------------------------------------
+
+sleep 5s
+
 pacman -S grub efibootmgr
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
@@ -12,3 +15,4 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo ----fim instalação do grub -------------------------------------------
+sleep 5s
