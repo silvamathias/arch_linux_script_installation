439f37be783bfef882baf123899495576ed5a13c
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 10:04:52 -0300

    incluindo tempos de espera

--- arch_install_scripts/03B_arch_instalacao_pacotes_xorg+virtualbox.sh
+++ arch_install_scripts/03B_arch_instalacao_pacotes_xorg+virtualbox.sh
@@ -3,8 +3,11 @@
 #instalando interface gráfica (xorg + Nvidia)
 echo instalando interface gráfica (xorg + Nvidia) --------------------------
 
+sleep 5s
+
 pacman -S xorg-server xorg-xinit xorg-apps mesa
 
 sudo pacman -S virtualbox-guest-utils
 
 echo ----fim da instalando interface gráfica (xorg + Nvidia) ---------------
+sleep 5s
