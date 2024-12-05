0acfbcce697a10ef3fd09a0308b7c57bf2e1c64c
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 13:42:37 -0300

    incluindo tempos de espera

--- arch_install_scripts/101_arch_instalacao_pacotes_tema_arch.sh
+++ arch_install_scripts/101_arch_instalacao_pacotes_tema_arch.sh
@@ -2,6 +2,9 @@
 
 #temas do archlinux
 echo instalando temas do archlinux -----------------------------------------
+sleep 5s
+
 pacman -S arc-gtk-theme arc-icon-theme arc-solid-gtk-theme archlinux-themes-slim keycloak-archlinux-theme archlinux-wallpaper
 
 echo ----fim instalação temas do archlinux ---------------------------------
+sleep 5s
