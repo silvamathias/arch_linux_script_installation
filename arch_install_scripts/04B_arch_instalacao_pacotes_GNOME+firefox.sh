a0adb209455f2029e3979325ec4332ebd88e02d7
Mathias <42849342+silvamathias@users.noreply.github.com>
2024-12-05 12:29:13 -0300

    incluindo tempos de espera

--- arch_install_scripts/04B_arch_instalacao_pacotes_GNOME+firefox.sh
+++ arch_install_scripts/04B_arch_instalacao_pacotes_GNOME+firefox.sh
@@ -3,9 +3,12 @@
 #pacotes para usar o Gnome
 echo Instalando pacotes Gnome ----------------------------------------------
 
+sleep 5s
+
 pacman -S gnome gnome-themes-extra firefox
 
 # Descomente a linha de baixo para instalar pacotes extras do gnome (jogos e outros apps)
 #pacman -S gnome-extra
 
 echo ----fim instalação pacotes Gnome --------------------------------------
+sleep 5s
